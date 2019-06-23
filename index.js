2381d581fb2d0d9b2adb620dc41c7941eadb77a0
steadfast-network-4426 <djreeves1292@gmail.com>
2019-06-23 03:52:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(name,holiday) {
+function happyHolidaysTo(name) {
   if (name) {
     return `Happy holidays, ${name}!`;
   }
-   if (name,holiday) {
-      return `Happy ${holiday}, ${name}!`;
-   }
-}  
-  
+}
+
+
 
 function holidayCountdown(holiday,days) {
   return `It's ${days} days until ${holiday}!`;
